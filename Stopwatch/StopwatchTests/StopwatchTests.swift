60897dd4fa892291acbc6ee0ceabbf20947037fe
vasillyev17 <65895677+vasillyev17@users.noreply.github.com>
2020-08-06 16:05:51 +0300

    Add files via upload

---
+++ Stopwatch/StopwatchTests/StopwatchTests.swift
@@ -0,0 +1,21 @@
+import XCTest
+
+class StopwatchTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+    }
+    
+    func testPerformanceExample() {
+        self.measure {
+        }
+    }
+    
+}
