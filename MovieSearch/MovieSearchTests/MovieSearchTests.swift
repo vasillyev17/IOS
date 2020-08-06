cbab0b72d2b627070a1f2a5adb6f58fbb9cc5cad
vasillyev17 <65895677+vasillyev17@users.noreply.github.com>
2020-08-06 16:01:20 +0300

    Add files via upload

---
+++ MovieSearch/MovieSearchTests/MovieSearchTests.swift
@@ -0,0 +1,22 @@
+import XCTest
+@testable import MovieSearch
+
+class MovieSearchTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+
+    }
+
+    func testExample() throws {
+
+    }
+
+    func testPerformanceExample() throws {
+        self.measure {
+        }
+    }
+
+}
---
+++ MovieSearch/MovieSearchUITests/MovieSearchUITests.swift
@@ -0,0 +1,27 @@
+import XCTest
+
+class MovieSearchUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+
+        continueAfterFailure = false
+
+    }
+
+    override func tearDownWithError() throws {
+
+    }
+
+    func testExample() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
